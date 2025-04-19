a55bd282394b84d456adff2027b47aa124ef9445
Mateo Ortega Vacas <dddmateo@gmail.com>
2025-04-19 15:32:34 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bike155.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
