66557a29f70cbfa319d67c49de607c11779aba81
Mateo Ortega Vacas <dddmateo@gmail.com>
2025-04-22 20:14:34 -0500

    Añadido Base de datos

---
+++ Migrations/20250423010532_Primera migracion.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bike155.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primeramigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.UsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ruta",
+                columns: table => new
+                {
+                    RutaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RutaDetalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RutaDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ruta", x => x.RutaId);
+                    table.ForeignKey(
+                        name: "FK_Ruta_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bike",
+                columns: table => new
+                {
+                    BikeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BakeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BakeDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RutaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bike", x => x.BikeId);
+                    table.ForeignKey(
+                        name: "FK_Bike_Ruta_RutaId",
+                        column: x => x.RutaId,
+                        principalTable: "Ruta",
+                        principalColumn: "RutaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bike_RutaId",
+                table: "Bike",
+                column: "RutaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ruta_UsuarioId",
+                table: "Ruta",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bike");
+
+            migrationBuilder.DropTable(
+                name: "Ruta");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
